791b2f2542b21b3f150738888996d091117567e0
anudeepvrm <anudeepvrm@gmail.com>
2015-11-09 14:48:53 +0530

    code1

---
+++ Door Mat.py
@@ -0,0 +1,11 @@
+__author__ = 'avarm1'
+
+
+
+N, M = (int(x) for x in input().split(" ")) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
+for i in range(1,N,2):
+    print("-"*int((M-(3*int(i)))/2)+".|."*i+"-"*int((M-(3*int(i)))/2))
+print("-"*int((M-7)/2)+"WELCOME"+"-"*int((M-7)/2))
+for i in range(N-2,-1,-2):
+    print("-"*int((M-(3*int(i)))/2)+".|."*i+"-"*int((M-(3*int(i)))/2))
+print(M)
